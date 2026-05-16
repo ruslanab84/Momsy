4dde1ef8e099dd535ab286e837949793e92f57fb
Ruslan Abdulov <rusikabdulov@gmail.com>
2026-05-16 22:52:37 +0400

    Initial Commit

---
+++ Momsy/MomsyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomsyApp.swift
+//  Momsy
+//
+//  Created by Ruslan Abdulov on 16.05.26.
+//
+
+import SwiftUI
+
+@main
+struct MomsyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
